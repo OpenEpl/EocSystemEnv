f8a3801a2ccea0114be22b27033e99d913668ea6
qiqiworld <1354092549@qq.com>
2019-08-12 17:12:34 +0800

    优化dll加载

--- env/lib/system/src/e/system/dll_core.h
+++ env/lib/system/src/e/system/dll_core.h
@@ -1,15 +1,15 @@
 #pragma once
 extern "C"
 {
-	__declspec(dllimport) intptr_t __stdcall LoadLibraryA(const char* lpLibFileName);
+	__declspec(dllimport) intptr_t __stdcall LoadLibraryW(const wchar_t* lpLibFileName);
 	__declspec(dllimport) void* __stdcall GetProcAddress(intptr_t hModule,const char* lpProcName);
 
 }
 #define eoc_DefineMoudleLoader(name, dllname) \
-static decltype(LoadLibraryA("")) InternalMoudleHandle_##name;\
-static decltype(LoadLibraryA("")) GetMoudleHandle_##name() {\
+static decltype(::LoadLibraryW(L"")) InternalMoudleHandle_##name;\
+static decltype(::LoadLibraryW(L"")) GetMoudleHandle_##name() {\
 	auto x = InternalMoudleHandle_##name;\
-	if(!x) InternalMoudleHandle_##name = x = LoadLibraryA(dllname);\
+	if(!x) InternalMoudleHandle_##name = x = ::LoadLibraryW(L ## dllname);\
 	return x;\
 }
 
@@ -17,6 +17,6 @@ static decltype(LoadLibraryA("")) GetMoudleHandle_##name() {\
 static void* InternalFuncPtr_##name;\
 static void* GetFuncPtr_##name() {\
 	void* x = InternalFuncPtr_##name;\
-	if (!x) InternalFuncPtr_##name = x = GetProcAddress(hmod, rname);\
+	if (!x) InternalFuncPtr_##name = x = ::GetProcAddress(hmod, rname);\
 	return x;\
 }
