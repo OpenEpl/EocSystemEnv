f63596f52626922d385b8084e37950ce18c12717
qiqiworld <1354092549@qq.com>
2020-06-14 18:15:14 +0800

    Fix CI broken

--- env/lib/system/test/test_native_string.cpp
+++ env/lib/system/test/test_native_string.cpp
@@ -2,9 +2,9 @@
 #include <e/system/native_string.h>
 TEST_CASE("native string", "[native_string]")
 {
-    auto normal = "世界您好";
-    auto wide = L"世界您好";
-    auto utf8 = u8"世界您好";
+    auto normal = "HelloWorld";
+    auto wide = L"HelloWorld";
+    auto utf8 = u8"HelloWorld";
     CHECK(wcscmp(e::system::ToNativeWideString(normal).get(), wide) == 0);
     CHECK(strcmp(e::system::ToNativeUtf8String(normal).get(), utf8) == 0);
     CHECK(e::system::ReceiveNativeWideString(wide) == normal);
